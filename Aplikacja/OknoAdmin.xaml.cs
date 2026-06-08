01e82b41d02401516ec6c6fdcd77dc0bcc16f3de
jakubhoma1 <kubahomail@gmail.com>
2026-06-08 11:44:52 +0200

    panel admin dodanie oknosprzet dodanie

---
+++ Aplikacja/OknoAdmin.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikacja
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoAdmin.xaml
+    /// </summary>
+    public partial class OknoAdmin : Window
+    {
+        public OknoAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Wyloguj_Click(object sender, RoutedEventArgs e)
+        {
+            OknoWyboruLogowania powrotDoOknaWyboruLogowania = new OknoWyboruLogowania();
+            powrotDoOknaWyboruLogowania.Show();
+            this.Hide();
+
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Sprzet_Click(object sender, RoutedEventArgs e)
+        {
+            OknoSprzet oknosprzetu = new OknoSprzet();
+            oknosprzetu.Show();
+            this.Hide();
+        }
+    }
+}
