e736c367cbbfdaeb6fa73d6f145c24dc9ff5955f
Mhomaa <mateuszhomail2@gmail.com>
2026-06-16 18:14:55 +0200

    male zmiany

--- Aplikacja/AppDbContext.cs
+++ Aplikacja/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace Aplikacja
 {
     public class AppDbContext : DbContext
     {
-        // Beda reprezentowac tabele w bazie danych
+
         public DbSet<Pracownik> Pracownicy { get; set; }
         public DbSet<Klient> Klienci { get; set; }
         public DbSet<Sprzet> Sprzety { get; set; }
@@ -16,7 +16,7 @@ namespace Aplikacja
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Wskazujemy, że chcemy używać bazy danych SQLite i podajemy nazwę pliku bazy danych
+           
             optionsBuilder.UseSqlite("Data Source=FirmaSprzatajaca.db");
         }
     }
